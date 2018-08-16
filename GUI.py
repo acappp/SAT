69d30ca032a59709eeec289dfb88ba5c9757ad29
Capp <19acappellucci@mlmc.vic.edu.au>
2018-08-16 15:02:57 +1000

    Started commenting my code, the remove function is now working. The program can now handle blank lines without crashing if they get into the file and I've started work on my sorting functions

--- GUI.py
+++ GUI.py
@@ -1,15 +1,29 @@
+"""
+Author: Anthony Cappellucci
+Date: xx/xx/2018
+Version 1.0
+
+This program is a movie cataloger that allows the user to enter the names and details of movies
+and store them inside of a sortable database.
+"""
+
+
 import tkinter as tk
 top = tk.Tk()
 
+# Setting the title of the initial window, setting it's size
+# and preventing it from being resizable by the user
 top.title('Movie Database')
 top.resizable(False, False)
 top.geometry('750x400')
 
 
-bgc = "#515151" # default background colour (dark grey)
-hdc = "#272727" # default header colour (darker grey)
-lightbgc = "#CDCDCD" # light grey background colour (for light theme)
-lighthdc = "#AAAAAA" # light grey header colour (for light theme)
+# Setting all colours as callable string value
+# so I can just say 'Black' if I want something to appear black
+bgc = "#515151" # dark grey
+hdc = "#272727" # darker grey
+lightbgc = "#CDCDCD" # light grey background colour
+lighthdc = "#AAAAAA" # light grey header colour
 Red = "#ff0000" # red
 Orange = "#ff7f00" # orange
 Yellow = "#ffff00" # yellow
@@ -17,14 +31,15 @@ Green = "#00ff00" # green
 Blue = "#0000ff" # blue
 Indigo = "#4b0082" # indigo
 Violet = "#9400d3" # violet
-LightBlue = "#C9FFFF"
-Black = "#000000"
-White = "#FFFFFF"
+LightBlue = "#C9FFFF" # Light Blue
+Black = "#000000" #Black
+White = "#FFFFFF" # White
 Pink = "#ff40e7" # Pink
 
 
 
 class Alldata:
+    # This Function calls all values entered from the user and adds them to a list of lists
     def __init__(self, name, director, rating, yearOfRelease, description):
         self.name = name
         self.director = director
@@ -32,9 +47,10 @@ class Alldata:
         self.yearOfRelease = yearOfRelease
         self.description = description
 
+    # This function writes all the values to the file,
+    # The '@' symbol is used as seperation because it is not commonly used in entries
     def write(self):
         f = open('database', 'a')
-        f.write('\n')
         f.write(self.name)
         f.write("@")
         f.write(self.director)
@@ -44,29 +60,49 @@ class Alldata:
         f.write(self.yearOfRelease)
         f.write("@")
         f.write(self.description)
-
         f.close()
 
 
+def clearfile():
+    f = open('database', 'w')
+    f.close()
+
+
+def ratesort():
+   # mRating = []
+    for i in Movies:
+        n = i.rating
+        ratingsorted = n.sort()
+        print(ratingsorted)
+        return ratingsorted
+
+
+
+# This is where the entries are stored alogside the file
 Movies = []
 
 
+# The purpose of this function is to read the file and add all previous entries into the list
+# Entries in the list do not save when the program closes so this is necessary if you wwant the entries
+# To save
+
 def startread():
     p = open('database', 'r')
+    q=0
     for line in p:
-        slist = line.split("@")
-        Movie = Alldata(slist[0], slist[1], slist[2], slist[3], slist[4])
-        Movies.append(Movie)
+        if line == '\n':
+            q=6
+        # If it's a new line skip the code, if it isn't don't skip
+        else:
+            slist = line.split("@")
+            Movie = Alldata(slist[0], slist[1], slist[2], slist[3], slist[4])
+            Movies.append(Movie)
 
     p.close()
     return Movies
 
 
-
-year = ["2017", "2018", "2019"]
-#tkvar = tk.StringVar(top)
-
-
+# This is the function for the edit menu
 def editmenu():
     edit = tk.Tk()
     edit.title("Edit an Existing entry")
@@ -76,47 +112,43 @@ def editmenu():
     header = tk.Frame(edit, bg=LightBlue, height=50, width=1000)
     header.pack()
     header.place(x=0, y=0)
-
     F1 = tk.Frame(edit, bg=Black, height=5, width=1000)
     F1.pack()
     F1.place(x=0, y=45)
-
     L1 = tk.Label(edit, text="Edit an Entry:")
     L1.pack()
-    L1.configure(bg=LightBlue)
-    L1.place(x=200, y=15)
-
-    L2 = tk.Label(edit, text="Enter the name of the entry you wish to change:")
+    L1.configure(bg=LightBlue, font = ("Futura", 20))
+    L1.place(x=200, y=10)
+    L2 = tk.Label(edit, text="Select the name of the entry you wish to change:")
     L2.pack()
     L2.place(x = 100, y = 50)
 
+
     ## For drop the box
-    #def generateEditlist():
-    #    mNames = []
-    #    for i in Movies:
-    #        mNames.append(i.name)
-    #    tkvar = tk.StringVar(edit)
-    #    tkvar.set(mNames[0])
+    def generatelist():
+        mNames = []
+        for i in Movies:
+            mNames.append(i.name)
+        tkvar = tk.StringVar(edit)
+        tkvar.set(mNames[0])
 
-    #    DropMenu = tk.OptionMenu(edit, tkvar, *mNames)
-    #    DropMenu.pack()
-    #    DropMenu.place(x=125, y=75)
-    #    DropMenu.configure(height=2, width=30)
+        DropMenu = tk.OptionMenu(edit, tkvar, *mNames)
+        DropMenu.pack()
+        DropMenu.place(x=125, y=75)
+        DropMenu.configure(height=2, width=30)
 
-    #    return tkvar
+        return tkvar
 
-    #def edit(tkvar):
-    #    e = tkvar.get()
-    #    print(e)
-    #    Movies.pop(0)
+    def delete(tkvar):
+        p = tkvar.get()
+        print(p)
+        Movies.pop(0)
 
-    # Remove entry command .remove
+    tkvar = generatelist()
 
-    #tkvar = generateEditlist()
-
-    #DeleteButton = tk.Button(edit, text="Delete", command=lambda: edit(tkvar))
-    #DeleteButton.pack()
-    #DeleteButton.place(x=300, y=350)
+    editButton = tk.Button(edit, text="Edit Entry", command=lambda: delete(tkvar))
+    editButton.pack()
+    editButton.place(x=275, y=350)
 
     DoneButton = tk.Button(edit, text="Done", command=edit.destroy)
     DoneButton.pack()
@@ -138,8 +170,8 @@ def removeEntry():
 
     L1 = tk.Label(remove, text="Remove an Entry:")
     L1.pack()
-    L1.configure(bg=LightBlue)
-    L1.place(x=200, y=15)
+    L1.configure(bg=LightBlue, font = ("Futura", 20))
+    L1.place(x=200, y=10)
 
     L2 = tk.Label(remove, text="Select the name of the entry you wish to remove:")
     L2.pack()
@@ -163,7 +195,18 @@ def removeEntry():
     def delete(tkvar):
         p = tkvar.get()
         print(p)
-        Movies.pop(0)
+        y=0
+        for i in Movies:
+
+            if i.name == p:
+                Movies.pop(y)
+            y+=1
+
+        clearfile()
+        for i in Movies:
+
+            i.write()
+
 
     # Remove entry command .remove
 
@@ -194,7 +237,7 @@ def addEntry():
 
     Title = tk.Label(add, text="Enter the details of your entry:")
     Title.pack()
-    Title.configure(bg=LightBlue)
+    Title.configure(bg=LightBlue, font = ("Futura", 20))
     Title.place(x=275, y=15)
 
     L1 = tk.Label(add, text="Name:")
@@ -257,6 +300,8 @@ def addEntry():
 
 
 def getentry(E1, E2, E3, E4, E5):
+    # The purpose of this while loop is to prevent the user from adding entries that are blank,
+    # If a blank line is detected by the start read function the program will crash
     repeat = True
     while repeat == True:
         name = E1.get()
@@ -272,6 +317,7 @@ def getentry(E1, E2, E3, E4, E5):
 
             repeat = False
 
+            # These clear the entry boxes once the entry has been added to the database
             E1.delete(0, 'end')
             E2.delete(0, 'end')
             E3.delete(0, 'end')
@@ -348,14 +394,11 @@ def database():
     DoneButton.place(x=900, y=350)
 
 
-#    ysortButton = tk.Button(data, text="Sort via Year", command = lambda: yearSort(i.yearOfRelease))
-#    ysortButton.pack()
-#    ysortButton.place(x=50, y = 10)
-#    ysortButton.configure(highlightbackground = LightBlue)
+    sortButton = tk.Button(data, text="Sort via Rating", command=ratesort)
+    sortButton.pack()
+    sortButton.place(x=50, y = 10)
+    sortButton.configure(highlightbackground = LightBlue)
 
-
-# def yearSort():
-#        yearOfRelease.sort()
 
 
 def settings():
@@ -370,8 +413,15 @@ def settings():
     DoneButton.place(x=375, y=350)
 
 
+# starts the start read function,
+# as explained before it adds all the previous entries entered by the user back into the movies list
+# It is necessary for this function to run before everything else
 startread()
 
+
+
+# These first three blocks are simply adding the frames and titles for the main menu of the GUI
+# I've used .place as I believe it's the most accuracte way of placing things
 t = tk.Frame(top, bg = LightBlue, height = 1000, width = 1000)
 t.pack()
 t.place(x = 0, y = 0)
@@ -380,13 +430,16 @@ bottomf = tk.Frame(top, bg = Black, height = 500, width = 750)
 bottomf.pack()
 bottomf.place(x = 0, y = 55)
 
-
 title = tk.Label(top, text="MOVIE PLANNER")
 title.pack()
 title.configure(bg = LightBlue, font = ("Futura", 30))
 title.place(x = 300, y = 12)
 
 
+
+
+# The next 6 small blocks of code here add all the needed buttons onto the main menu of the GUI
+# Each calls a different fuction that will open a specific corresponding  window
 settingsbutton = tk.Button(top, text="Settings", fg = "Black", command = settings)
 settingsbutton.pack()
 settingsbutton.place(x = 650, y = 12)
@@ -408,15 +461,16 @@ Editbutton = tk.Button(top, text="Edit Entry", fg="black", height = 10, width = 
 Editbutton.pack()
 Editbutton.place(x=0, y=230)
 
-VSbutton = tk.Button(top, text="View Schedule", fg="black", height = 10, width = 30 ) #command = scheduleMenu)
-VSbutton.pack()
-VSbutton.place(x=270, y=230)
+#VSbutton = tk.Button(top, text="View Schedule", fg="black", height = 10, width = 30 ) #command = scheduleMenu)
+#VSbutton.pack()
+#VSbutton.place(x=270, y=230)
 
 Closebutton = tk.Button(top, text="Close", fg= "Black", height = 10, width = 23, command = quit)
 Closebutton.pack()
 Closebutton.place(x=540, y=230)
 
 
+# This frame is the black line seperating the title from the buttons
 f = tk.Frame(top, bg = Black, height = 8, width = 750)
 f.pack()
 f.place(x = 0, y = 50)
